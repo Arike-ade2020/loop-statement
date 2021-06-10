f9da676ee79270a70a336317da308f9a20211e80
Arike-ade2020 <mansuratabdulraheem2018@gmail.com>
2021-06-10 14:23:45 +0100

    first commit

---
+++ loop.py
@@ -0,0 +1,10 @@
+msg = input("say somthing:")
+i = 1
+while msg !="stop copying me":
+    if i<3:
+        print (msg)
+        msg = input()
+        i = i +3
+else: 
+    print ("UGH FINE YOU WIN, BROTHER!")
+
